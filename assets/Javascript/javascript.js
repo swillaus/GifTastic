4d6556538d97069c67d2614f84c6ad54b73917b3
Your Name <sam@upsidebusinessgroup.com.au>
2019-08-20 08:07:27 -0400

    fixed up code so that added options now producing GIF. Updated styling

--- assets/Javascript/javascript.js
+++ assets/Javascript/javascript.js
@@ -1,37 +1,53 @@
 // create an empty array to contain button options
-var buttonOptionsArr = ["toys", "music"];
+var buttonOptionsArr = ["toys", "music", "cars","news","beach", "animals"];
+var apiKey = "CDx5tDHI18D6iz6dXOX3u0uKX00EsaSc"
 
-
-// on click of search button, take text and create a button within arrary for it 
+// function to display buttons on initial load
 function renderButtons() {
 
-    // Deleting the movie buttons prior to adding new movie buttons
-    // (this is necessary otherwise we will have repeat buttons)
+    // Ensure there are no repeats
     $("#buttonOptions").empty();
 
     // Looping through the array of movies
     for (var i = 0; i < buttonOptionsArr.length; i++) {
 
-        // Then dynamicaly generating buttons for each movie in the array.
-        // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
+        // Dynamicaly generating buttons for each GIF in the array.
         var a = $("<button>");
         // Adding a class
-        a.addClass("ButtonSection");
-        // Adding a data-attribute with a value of the movie at index i
+        a.addClass("ButtonSection btn btn-info");
+        // Adding a data-attribute with a value of the GIF at index i
         a.attr("data-name", buttonOptionsArr[i]);
-        // Providing the button's text with a value of the movie at index i
+        // Providing the button's text with a value of the GIF at index i
         a.text(buttonOptionsArr[i]);
         // Adding the button to the HTML
         $("#buttonOptions").append(a);
-
-        
     }
+
+    // implement AJAX function to render GIFs to div section
+    $(".ButtonSection").on("click", function () {
+        var x = $(this).data("name")
+        console.log(x);
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + x + "&api_key="+apiKey+"&limit=10"
+        $.ajax({ url: queryURL, method: "GET" })
+            .done(function (response) {
+                console.log(response)
+                for (i = 0; i < response.data.length; i++) {
+                    var imagesDiv = $("<div>");
+                    var p = $("<p>").text("Rating:" + response.data[i].rating);
+                    var GIFimg = $("<img>");
+                    GIFimg.attr("src", response.data[i].images.fixed_height.url);
+                    imagesDiv.append(GIFimg);
+                    imagesDiv.append(p);
+                    $("#imagesSection").append(imagesDiv)
+                }
+            })
+    })
 }
 
 
 // SW - Take the value from the search box and place it into the button array and display
 $("#searchButton").on("click", function (event) {
-    // event.preventDefault() prevents the form from trying to submit itself.
+
     // We're using a form so that the user can hit enter instead of clicking the button if they want
     event.preventDefault();
 
@@ -40,35 +56,20 @@ $("#searchButton").on("click", function (event) {
     // The movie from the textbox is then added to our array
     buttonOptionsArr.push(selection);
 
-    // calling renderButtons which handles the processing of our movie array
+    // calling renderButtons which handles the processing of our GIF array
     renderButtons();
 
     console.log(selection)
 
     console.log(buttonOptionsArr)
+
+    
 });
 
 
-// Calling the renderButtons function at least once to display the initial list of movies
+// Calling the renderButtons function at least once to display the initial list of GIFs
 renderButtons();
 
 //   Render GIF images based on button click
 
-$(".buttonSection").on("click", function () {
-    var x = $(this).data("name")
-    console.log(x);
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + x + "&api_key=CDx5tDHI18D6iz6dXOX3u0uKX00EsaSc&limit=10"
-    $.ajax({ url: queryURL, method: "GET" })
-        .done(function (response) {
-            console.log(response)
-            for (i = 0; i < response.data.length; i++) {
-                var imagesDiv = $("<div>");
-                var p = $("<p>").text("Rating:"+response.data[i].rating);
-                var GIFimg = $("<img>");
-                GIFimg.attr("src",response.data[i].images.fixed_height.url);
-                imagesDiv.append(p);
-                imagesDiv.append(GIFimg);
-                $("#imagesSection").append(imagesDiv)
-        }
-        })
-})
+
